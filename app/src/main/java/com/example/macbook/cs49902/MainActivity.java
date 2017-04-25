67c28dd25693b6229b627238ca1a13b09038178e
amanizamzami <ayzamzami@cpp.edu>
2017-04-24 18:03:37 -0700

    A3

--- app/src/main/java/com/example/macbook/cs49902/MainActivity.java
+++ app/src/main/java/com/example/macbook/cs49902/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.macbook.cs49902;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button button1;
+
+    public void login(){
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+
+        });
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login();
     }
 }
